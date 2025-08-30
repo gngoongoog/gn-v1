55f346dadb80ce609850f96ab405272ce96897c8
gngoongoog <95882142+gngoongoog@users.noreply.github.com>
2025-08-31 04:21:37 +0500

    index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import GnStore from './GnStore';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<GnStore />);
