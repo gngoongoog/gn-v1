3c6ec0bccfaff11c1a24a60d2a3994938f627868
gngoongoog <95882142+gngoongoog@users.noreply.github.com>
2025-08-31 04:20:57 +0500

    Delete src/src directory

--- src/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import GnStore from './GnStore';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<GnStore />);
